7a12e8aedbd41970fd830dbc368bed189aa7ed72
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-05-30 10:20:18 +0300

    Move proj files to front folder

---
+++ front/src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>This is an cmpe 496 project.</h1>
+    <p>This is an online shopping project with a little database with sqlite</p>
+    <p>There will be 10 furnitures listed. The users will login with a popup first.</p>
+    <p>Then they will select a furniture and pay with a credit card info.</p>
+    <p>The system will be able to sort the products according to their popularity.</p>
+    <p>There will be three types of credit card: Visa, MasterCard, Discover</p>
+    <p>The default value will be None for the credit card type</p>
+    <p>The users will also enter their email address when buying</p>
+    <p>There will be input validation in the user interface</p>
+  </div>
+</template>
